e73851d60a91f91286d7008538cafba28fed9049
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-03-21 18:49:36 +0000

    add catchall for undefined routes

--- server/lib/router.js
+++ server/lib/router.js
@@ -23,7 +23,9 @@ router.post("/login", async (req, res) => {
       msg: "Logged-in Succesfully",
       code: 200,
     });
-  } catch (error) {}
+  } catch (error) {
+    throw new Error(error);
+  }
 });
 
 // POST /logout
@@ -34,7 +36,22 @@ router.post("/logout", async (req, res) => {
       msg: "Logged-out Succesfully",
       code: 200,
     });
-  } catch (error) {}
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+// Catchall for Undefined Routes
+router.all("*", async (req, res) => {
+  try {
+    res.status(404).json({
+      timestamp: Date.now(),
+      msg: "Route Not Found",
+      code: 404,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
 });
 
 module.exports = router;
