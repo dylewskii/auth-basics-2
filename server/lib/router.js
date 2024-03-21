349ad354be3e9096e0b8c84e99e308169efd894f
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-03-21 18:43:22 +0000

    set up login/logout routes

--- server/lib/router.js
+++ server/lib/router.js
@@ -3,11 +3,35 @@ const express = require("express");
 const router = express.Router();
 
 // POST /register
-router.post("/register", async (req, res, next) => {
+router.post("/register", async (req, res) => {
   try {
     res.status(200).json({
       timestamp: Date.now(),
       msg: "Succesfully Registered",
+      code: 200,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+// POST /login
+router.post("/login", async (req, res) => {
+  try {
+    res.status(200).json({
+      timestamp: Date.now(),
+      msg: "Logged-in Succesfully",
+      code: 200,
+    });
+  } catch (error) {}
+});
+
+// POST /logout
+router.post("/logout", async (req, res) => {
+  try {
+    res.status(200).json({
+      timestamp: Date.now(),
+      msg: "Logged-out Succesfully",
       code: 200,
     });
   } catch (error) {}
