3b8cdeb2b7e2ba346b91ae20ae8c80e8afa39050
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-03-22 11:28:25 +0000

    remove comments

--- server/lib/router.js
+++ server/lib/router.js
@@ -6,20 +6,15 @@ const router = express.Router();
 // POST /register
 router.post("/register", async (req, res, next) => {
   try {
-    // console.log(req.body);
-    res.send("POST REGISTER");
-    // const { firstName, lastName, email, password } = req.body;
-    // console.log(firstName, lastName, email, password);
-    // next();
     let user = new User();
     user.setFirstName("First Test");
     user.setLastName("Last Test");
     user.setEmail("test@test.com");
     user.setPassword("password");
 
+    console.log(req.body);
     const { firstName, lastName, email, password } = req.body;
     console.log(firstName, lastName, email, password);
-    res;
   } catch (error) {
     throw new Error(error);
   }
