d0796498dd38fe589b180603c85625cd0cfcfe7f
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-03-26 10:45:04 +0000

    add fetch request to api endpoint

--- frontend/register.js
+++ frontend/register.js
@@ -13,4 +13,29 @@ document
       errMsgDiv.innerText = "All fields are required";
       return;
     }
+
+    try {
+      const response = await fetch("http://localhost:3000/api/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          firstName,
+          lastName,
+          email,
+          password,
+        }),
+      });
+
+      if (response.ok) {
+      } else {
+        const { error } = await response.json();
+        throw new Error(error?.message[0]);
+      }
+    } catch (error) {
+      errMsgDiv.innerText = `Failed to register user: ${
+        error.message ? error.message : "Unknown error occured"
+      }`;
+    }
   });
